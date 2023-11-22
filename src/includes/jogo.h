30b6d2a703703f47ef2315d8c751c02e6d5db059
João Pedro Araújo Nóbrega <130778610+JaumPyetro@users.noreply.github.com>
2023-11-22 18:53:41 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/Joaopedrobs7/PIFP1""
    This reverts commit adeb73dedf281e352b798fee9d4d7ef2968e5d20.

--- src/includes/jogo.h
+++ src/includes/jogo.h
@@ -7,6 +7,10 @@
     #include <unistd.h>
    #include "estruturas.h"
 
-    void shuffle(dado dado);
-
+    dado shuffle(dado dado);
+    void create_player(player* player);
+    void bet(player* player);
+    void saldo(player player);
+    void match(player* jogador,dado player,dado banker,char* winner);
+    void depositar(player* jogador);
 #endif  /*FILE_A*/
--- src/main.c
+++ src/main.c
@@ -4,17 +4,36 @@
 
 
 int main(){
-    listreference* head;
+    listreference* head = NULL;
     inithead(&head);
+    player jogador;
     dado player;
     dado banker;
+    char winner[10];
+    create_player(&jogador);
     
-    shuffle(player);
-    shuffle(banker);
+    
+    while(1){
+        bet(&jogador);
+        saldo(jogador);
+    
+        //girando os dados
+        player = shuffle(player);
+        banker = shuffle(banker);
+        // player.dado1 = 5;
+        // player.dado2 = 5;
+        // banker.dado1 = 6;
+        // banker.dado2 = 6;
+    
+        match(&jogador,player,banker,winner); 
+        printf("Vencedor = %s\n",winner);
+        insert(&head,winner);
+        printlist(&head);
+        saldo(jogador);    
+    }
     
     
-    printf("PLAYER:\nDado 1 = %d\nDado 2 = %d\nBanker:\nDado 1 = %d\nDado 2 = %d",player.dado1,player.dado2,banker.dado1,banker.dado2);
-   
+
     
    
     
